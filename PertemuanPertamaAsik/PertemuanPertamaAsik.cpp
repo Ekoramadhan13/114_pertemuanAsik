5f3dfc130e9ece30a765bc0008cc663f785c436f
Ekoramadhan13 <enugraha411@gmail.com>
2024-02-26 11:57:11 +0700

    menghitung dan menampilkan luas

--- PertemuanPertamaAsik/PertemuanPertamaAsik.cpp
+++ PertemuanPertamaAsik/PertemuanPertamaAsik.cpp
@@ -5,5 +5,19 @@ int main()
 {
     cout << "Hello World!"<< endl;
     cout << "ucup lebay"<< endl;
+    //begin
+        // numeric njejari, nluas
+        // display  'masukan jejari= '
+        // accept njejari
+        // compute nluas = 3.14 # r # r
+        // display 'luasnya= ' + nluas
+    //end 
 
+    double njejari, nluas;
+    cout << "masukan jejari = ";
+    cin >> njejari;
+    nluas = 3, 14 * njejari * njejari;
+    cout << "luasnya = " << nluas << endl;
+
+    
 }
